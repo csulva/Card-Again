a0ff0b5e283ab6b7f0aeec0dc7c4156c52ea4467
csulva <chrissulva1@gmail.com>
2021-11-23 18:04:35 -0500

    new config file

---
+++ config.py
@@ -0,0 +1,90 @@
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY') or "anyrandomstringofstrings#123"
+
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+
+    CARDAGAIN_ADMIN = os.environ.get('CARDAGAIN_ADMIN')
+    CARDAGAIN_MAIL_SUBJECT_PREFIX = 'Card Again - '
+    CARDAGAIN_MAIL_SENDER = f'Card Again Admin <{CARDAGAIN_ADMIN}>'
+
+    CARDAGAIN_COMPS_PER_PAGE = 20
+    CARDAGAIN_FOLLOWERS_PER_PAGE = 20
+
+    HTTPS_REDIRECT = False
+
+    @staticmethod
+    def init_app(app):
+        pass
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_DEV_URL') or \
+        f'sqlite:///{os.path.join(basedir, "data-dev.sqlite")}'
+
+class TestingConfig(Config):
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_TEST_URL') or \
+        f'sqlite:///{os.path.join(basedir, "data-test.sqlite")}'
+
+class ProductionConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        f'sqlite:///{os.path.join(basedir, "data.sqlite")}'
+
+    @classmethod
+    def init_app(cls, app):
+        Config.init_app(app)
+
+        import logging
+        from logging.handlers import SMTPHandler
+        creds = None
+        secure = None
+        if getattr(cls, 'MAIL_USERNAME', None) is not None:
+            creds = (cls.MAIL_USERNAME, cls.MAIL_PASSWORD)
+            if getattr(cls, 'MAIL_USE_TLS', None):
+                # logging: to use TLS, must pass tuple (can be empty)
+                secure = ()
+        mail_handler = SMTPHandler(
+            mailhost=(cls.MAIL_SERVER, cls.MAIL_PORT),
+            fromaddr=cls.CARDAGAIN_MAIL_SENDER,
+            toaddrs=[cls.CARDAGAIN_ADMIN],
+            subject=cls.CARDAGAIN_MAIL_SUBJECT_PREFIX + " Application Error",
+            credentials=creds,
+            secure=secure
+        )
+        mail_handler.setLevel(logging.ERROR)
+        app.logger.addHandler(mail_handler)
+
+class HerokuConfig(ProductionConfig):
+    HTTPS_REDIRECT = True if os.environ.get('DYNO') else False
+
+    @classmethod
+    def init_app(cls, app):
+        ProductionConfig.init_app(app)
+
+        # log to stderr
+        import logging
+        from logging import StreamHandler
+        file_handler = StreamHandler
+        file_handler.setLevel(file_handler, level=logging.INFO)
+        app.logger.addHandler(file_handler)
+
+        from werkzeug.middleware.proxy_fix import ProxyFix
+        app.wsgi_app = ProxyFix(app.wsgi_app)
+
+config = {'development': DevelopmentConfig,
+'testing': TestingConfig,
+'production': ProductionConfig,
+'default': DevelopmentConfig,
+'heroku': HerokuConfig,
+}
