05b02cc9c7efca355437e821b3992e21ec795b09
csulva <chrissulva1@gmail.com>
2021-11-23 19:32:40 -0500

    auth blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,20 @@
 from flask import Flask
 from config import config
+from flask_bootstrap import Bootstrap
 
+bootstrap = Bootstrap()
 
 def create_app(config_name='default'):
     app = Flask(__name__)
     app.config.from_object(config[config_name])
     config[config_name].init_app(app)
 
+    bootstrap.init_app(app)
+
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
+
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint)
 
     return app
