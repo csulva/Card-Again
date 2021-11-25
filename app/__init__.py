e12ac48ba9d42707bb095edcf14e9a0cbfdb6e2d
csulva <chrissulva1@gmail.com>
2021-11-24 22:45:09 -0500

    added db

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,19 @@
 from flask import Flask
+import sqlalchemy
 from config import config
 from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 bootstrap = Bootstrap()
+db = SQLAlchemy()
 
 def create_app(config_name='default'):
     app = Flask(__name__)
     app.config.from_object(config[config_name])
     config[config_name].init_app(app)
+
+    db.init_app(app)
 
     bootstrap.init_app(app)
 
