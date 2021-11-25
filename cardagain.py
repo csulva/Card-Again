6bbebd2a5ff3fb81f377ed9d4bdb3df80ea968a3
csulva <chrissulva1@gmail.com>
2021-11-24 22:45:42 -0500

    shell_context_processor

--- cardagain.py
+++ cardagain.py
@@ -1,4 +1,12 @@
-from app import create_app
+from app import create_app, db
 import os
+from flask_migrate import Migrate
+from app.models import User
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
+
+migrate = Migrate(app, db, render_as_batch=True)
+
+@app.shell_context_processor
+def make_shell_context():
+    return dict(db=db, User=User)
