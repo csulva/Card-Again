8a3504cdc0629bcf9d7e5c7a785d161679c40763
csulva <chrissulva1@gmail.com>
2021-11-26 14:46:49 -0500

    new card table in progress

--- app/models.py
+++ app/models.py
@@ -9,5 +9,16 @@ class User(db.Model):
     email = db.Column(db.String(128), index=True, unique=True)
     password_hash = db.Column(db.String(128))
 
+    cards = db.relationship('Card', backref='owner', lazy='dynamic')
+
     def __repr__(self) -> str:
         return '<User {}>'.format(self.username)
+
+class Card(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    # change this
+    # def __repr__(self):
+    #     return '<Post {}>'.format(self.body)
