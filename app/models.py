d1fee13aee827e1202505eb5a1d8d7ea3bd4208b
csulva <chrissulva1@gmail.com>
2021-11-26 16:00:43 -0500

    adding user loader and password hashing

--- app/models.py
+++ app/models.py
@@ -1,9 +1,10 @@
-from app import db
+from app import db, login
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
 
 
 
-
-class User(db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(128), index=True, unique=True)
@@ -14,6 +15,13 @@ class User(db.Model):
     def __repr__(self) -> str:
         return '<User {}>'.format(self.username)
 
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+
 class Card(db.Model):
     id = db.Column(db.Integer, primary_key=True)
 
@@ -22,3 +30,8 @@ class Card(db.Model):
     # change this
     # def __repr__(self):
     #     return '<Post {}>'.format(self.body)
+
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
