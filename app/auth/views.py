c9c60fc0662aee98ef12470809c93f45214aee4d
csulva <chrissulva1@gmail.com>
2021-11-29 18:04:22 -0500

    updates to view functions

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,6 +1,7 @@
 from . import auth
+from app import db
 from flask import render_template, url_for, redirect, flash, request
-from .forms import LoginForm
+from .forms import LoginForm, RegistrationForm
 from flask_login import current_user, login_user, logout_user, login_required
 from app.models import User
 from werkzeug.urls import url_parse
@@ -20,7 +21,7 @@ def login():
         if not next_page or url_parse(next_page).netloc != '':
             next_page = url_for('main.index')
         return redirect(next_page)
-    return render_template('auth/login.html', title='Sign In', form=form)
+    return render_template('auth/login.html', title='Log Into Card Again', form=form)
 
 @auth.route('/logout')
 def logout():
@@ -29,6 +30,26 @@ def logout():
 
 @auth.route('/register', methods=["GET", "POST"])
 def register():
-
-
-    return render_template('auth/register.html')
+    form = RegistrationForm()
+    if current_user.is_authenticated:
+        return redirect(url_for('main.index'))
+    if form.validate_on_submit():
+        email_entered = form.email.data
+        username_entered = form.username.data
+        password_entered = form.password.data
+        user = User.query.filter_by(username=username_entered).first()
+        if user is None:
+            user = User(username=username_entered, email=email_entered)
+            user.set_password(password_entered)
+            db.session.add(user)
+            db.session.commit()
+            # token = user.generate_confirmation_token()
+            # confirmation_link = url_for('auth.confirm', token=token, _external=True)
+            # send_email(user.email, 'Welcome to Ragtime!', 'mail/welcome', user=user)
+            # send_email(user.email, 'Confirm your account with Ragtime', 'auth/confirm',  confirmation_link=confirmation_link)
+            # send_email('chrservices15@gmail.com', 'A new user has been created!', 'mail/new_user', user=user)
+            flash('Thanks for registering!')
+            return redirect(url_for('auth.login'))
+        else:
+            return redirect(url_for('auth.register'))
+    return render_template('auth/register.html', form=form, title='Register for Card Again')
--- app/main/views.py
+++ app/main/views.py
@@ -2,10 +2,11 @@ from flask_login.utils import login_required
 from app.auth.views import login
 from . import main
 from flask import render_template, url_for
+from flask_login import current_user
 
 @main.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', user=current_user)
 
 @main.route('/test')
 @login_required
