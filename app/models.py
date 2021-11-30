9a39eac817fbdd580b12e58d613907af8560404f
csulva <chrissulva1@gmail.com>
2021-11-30 18:09:43 -0500

    new roles table, permissions, and role_id to user table

--- app/models.py
+++ app/models.py
@@ -3,6 +3,7 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from hashlib import md5
 from datetime import datetime
+from flask import current_app
 
 
 class User(UserMixin, db.Model):
@@ -10,6 +11,7 @@ class User(UserMixin, db.Model):
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(128), index=True, unique=True)
     password_hash = db.Column(db.String(128))
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
 
     about_me = db.Column(db.String(140))
     last_seen = db.Column(db.DateTime, default=datetime.utcnow)
@@ -33,6 +35,30 @@ class User(UserMixin, db.Model):
         digest = md5(self.email.lower().encode('utf-8')).hexdigest()
         return f'https://www.gravatar.com/monsterid/{digest}?d=identicon&s={size}'
 
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        if self.role is None:
+            if self.username == current_app.config['CARDAGAIN_ADMIN']:
+                self.role = Role.query.filter_by(name='Administrator').first()
+            if self.role is None:
+                self.role = Role.query.filter_by(default=True).first()
+        # if self.email is not None and self.avatar_hash is None:
+        #     self.avatar_hash = self.email_hash()
+        # self.follow(self)
+
+    def can(self, perm):
+        return self.role is not None and self.role.has_permission(perm)
+
+    def is_administrator(self):
+        return self.can(Permission.ADMIN)
+
+    @staticmethod
+    def make_new_users_user_role():
+        for u in User.query.all():
+            if u.role == None:
+                u.role = Role.query.filter_by(default=True).first()
+                db.session.commit()
+
 
 class Card(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -47,3 +73,72 @@ class Card(db.Model):
 @login.user_loader
 def load_user(id):
     return User.query.get(int(id))
+
+class Permission:
+    FOLLOW = 1
+    REVIEW = 2
+    PUBLISH = 4
+    MODERATE = 8
+    ADMIN = 16
+
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    users = db.relationship('User', backref='role', lazy='dynamic')
+    default = db.Column(db.Boolean, default=False, index=True)
+    permissions = db.Column(db.Integer)
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        if self.permissions is None:
+            self.permissions = 0
+
+    def __repr__(self):
+        return f'<Role: {self.name}>'
+
+    def add_permission(self, perm):
+        if not self.has_permission(perm):
+            self.permissions += perm
+
+    def remove_permission(self, perm):
+        if self.has_permission(perm):
+            self.permissions -= perm
+
+    def reset_permissions(self):
+        self.permissions = 0
+
+    def has_permission(self, perm):
+        return self.permissions & perm == perm
+
+    @staticmethod
+    def insert_roles():
+        roles = {
+            'User':             [Permission.FOLLOW,
+                                 Permission.REVIEW,
+                                 Permission.PUBLISH],
+            'Moderator':        [Permission.FOLLOW,
+                                 Permission.REVIEW,
+                                 Permission.PUBLISH,
+                                 Permission.MODERATE],
+            'Administrator':    [Permission.FOLLOW,
+                                 Permission.REVIEW,
+                                 Permission.PUBLISH,
+                                 Permission.MODERATE,
+                                 Permission.ADMIN],
+        }
+        default_role = 'User'
+        for r in roles:
+            # see if role is already in table
+            role = Role.query.filter_by(name=r).first()
+            if role is None:
+                # it's not so make a new one
+                role = Role(name=r)
+            role.reset_permissions()
+        # add whichever permissions the role needs
+            for perm in roles[r]:
+                role.add_permission(perm)
+            # if role is the default one, default is True
+            role.default = (role.name == default_role)
+            db.session.add(role)
+        db.session.commit()
