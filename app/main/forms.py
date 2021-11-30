b1d6fd856c33352c4be8b489b9306558d7017099
csulva <chrissulva1@gmail.com>
2021-11-30 00:33:56 -0500

    edit profile views and forms

---
+++ app/main/forms.py
@@ -0,0 +1,10 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, TextAreaField, SubmitField
+from wtforms.validators import DataRequired, Length
+from app.models import User
+
+
+class EditProfileForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    about_me = TextAreaField('About me', validators=[Length(min=0, max=140)])
+    submit = SubmitField('Submit')
--- app/main/views.py
+++ app/main/views.py
@@ -1,8 +1,8 @@
 from flask_login.utils import login_required
 from app import db
-from app.auth.views import login
+from app.main.forms import EditProfileForm
 from . import main
-from flask import render_template, url_for
+from flask import render_template, url_for, flash, redirect, request
 from flask_login import current_user
 from app.models import User, Card
 from datetime import datetime
@@ -28,3 +28,19 @@ def before_request():
     if current_user.is_authenticated:
         current_user.last_seen = datetime.utcnow()
         db.session.commit()
+
+@main.route('/edit-profile', methods=["GET", "POST"])
+@login_required
+def edit_profile():
+    form = EditProfileForm()
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.about_me = form.about_me.data
+        db.session.commit()
+        flash('Your changes have been saved.')
+        return redirect(url_for('main.edit_profile'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.about_me.data = current_user.about_me
+    return render_template('edit_profile.html', title='Edit Profile',
+                           form=form)
