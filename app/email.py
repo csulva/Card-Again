6f2972f28f81f08c5f0dffca99a7ce3e5367ebe8
csulva <chrissulva1@gmail.com>
2021-12-01 18:34:09 -0500

    email functionality and templates

---
+++ app/email.py
@@ -0,0 +1,21 @@
+from flask import current_app, render_template
+from flask_mail import Message
+from . import mail
+from threading import Thread
+
+def send_async_email(app, msg):
+    with app.app_context():
+        mail.send(msg)
+
+def send_email(to, subject, template, **kwargs):
+    app = current_app._get_current_object()
+    msg = Message(
+        subject=app.config['CARDAGAIN_MAIL_SUBJECT_PREFIX'] + subject,
+        recipients=[to],
+        sender=app.config['CARDAGAIN_MAIL_SENDER']
+    )
+
+    msg.body = render_template(template + '.txt', **kwargs)
+    msg.html = render_template(template + '.html', **kwargs)
+    thread = Thread(target=send_async_email, args=[app, msg])
+    thread.start()
