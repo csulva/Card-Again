e458d147e61d86ae396a592711aaf3fe3e3ab85b
csulva <chrissulva1@gmail.com>
2021-12-03 00:04:34 -0500

    follow and unfollow functions and templates

--- app/main/views.py
+++ app/main/views.py
@@ -4,8 +4,9 @@ from app.main.forms import EditProfileForm
 from . import main
 from flask import render_template, url_for, flash, redirect, request
 from flask_login import current_user
-from app.models import User, Card
+from app.models import User, Card, Permission
 from datetime import datetime
+from ..decorators import permission_required
 
 @main.route('/')
 def index():
@@ -44,3 +45,35 @@ def edit_profile():
         form.about_me.data = current_user.about_me
     return render_template('edit_profile.html', title='Edit Profile',
                            form=form)
+
+@main.route('/follow/<username>')
+@login_required
+@permission_required(Permission.FOLLOW)
+def follow(username):
+    user = User.query.filter_by(username=username).first()
+    if user is None:
+        flash("That is not a valid user.")
+        return redirect(url_for('.index'))
+    if current_user.is_following(user):
+        flash("Looks like you are already following that user.")
+        return redirect(url_for('.user', username=username))
+    current_user.follow(user)
+    db.session.commit()
+    flash(f"You are now following {username}")
+    return redirect(url_for('.user', username=username))
+
+@main.route('/unfollow/<username>')
+@login_required
+@permission_required(Permission.FOLLOW)
+def unfollow(username):
+    user = User.query.filter_by(username=username).first()
+    if user is None:
+        flash("That is not a valid user.")
+        return redirect(url_for('.index'))
+    if not current_user.is_following(user):
+        flash("Looks like you aren't already following that user.")
+        return redirect(url_for('.user', username=username))
+    current_user.unfollow(user)
+    db.session.commit()
+    flash(f"You have successfully unfollowed {username}.")
+    return redirect(url_for('.user', username=username))
