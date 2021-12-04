51433337d12d6de057105d9ecbfd7cbd941fc67d
csulva <chrissulva1@gmail.com>
2021-12-03 22:59:25 -0500

    new edit profile form name change

--- app/main/forms.py
+++ app/main/forms.py
@@ -5,16 +5,7 @@ from app.models import User
 
 
 class EditProfileForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
+    name = StringField('Name', validators=[Length(min=0, max=64)])
     about_me = TextAreaField('About me', validators=[Length(min=0, max=140)])
     submit = SubmitField('Submit')
 
-    def __init__(self, original_username, *args, **kwargs):
-        super(EditProfileForm, self).__init__(*args, **kwargs)
-        self.original_username = original_username
-
-    def validate_username(self, username):
-        if username.data != self.original_username:
-            user = User.query.filter_by(username=self.username.data).first()
-            if user is not None:
-                raise ValidationError('That username is taken. Please try a different username.')
--- app/main/views.py
+++ app/main/views.py
@@ -33,15 +33,15 @@ def before_request():
 @main.route('/edit-profile', methods=["GET", "POST"])
 @login_required
 def edit_profile():
-    form = EditProfileForm(current_user.username)
+    form = EditProfileForm()
     if form.validate_on_submit():
-        current_user.username = form.username.data
+        current_user.name = form.name.data
         current_user.about_me = form.about_me.data
         db.session.commit()
         flash('Your changes have been saved.')
         return redirect(url_for('main.edit_profile'))
     elif request.method == 'GET':
-        form.username.data = current_user.username
+        form.name.data = current_user.name
         form.about_me.data = current_user.about_me
     return render_template('edit_profile.html', title='Edit Profile',
                            form=form)
