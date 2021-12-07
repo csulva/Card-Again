ddf02bc9cbde9b2f720c6bf2d9d4fbb8823fcf5c
csulva <chrissulva1@gmail.com>
2021-12-07 15:04:20 -0500

    getting api data for ids

--- API/load.py
+++ API/load.py
@@ -2,17 +2,36 @@ from bs4 import BeautifulSoup
 import requests
 import json
 
-BASE_URL = f'https://api.pokemontcg.io/v2/cards'
-# id = 'xy5-1'
-request = requests.get(BASE_URL)
-data = request.json()
-
 list_of_ids = []
-for key, value in data.items():
-    if key == 'data':
-        for x in key:
-            for data_key, data_value in x.items():
-                if data_key == 'id':
-                    list_of_ids.append(data_value)
+num = 1
+while num < 59:
+    BASE_URL = f'https://api.pokemontcg.io/v2/cards/?search=a&page={num}'
+    request = requests.get(BASE_URL)
+    data = request.json()
+    for key, value in data.items():
+        if key == 'data':
+            for dicts in value:
+                for key_data, value_data in dicts.items():
+                    if key_data == 'id':
+                        list_of_ids.append(value_data)
+    num += 1
 
-print(list_of_ids[0])
+
+with open('card_ids.json', 'w') as fout:
+    json.dump(list_of_ids, fout)
+
+    # if key == 'data':
+    #     for x in key:
+    #         for data_key, data_value in x.items():
+    #             if data_key == 'id':
+    #                 list_of_id.append(data_value)
+
+# list_of_ids = []
+# for key, value in data.items():
+#     if key == 'data':
+#         for x in key:
+#             for data_key, data_value in x.items():
+#                 if data_key == 'id':
+#                     list_of_ids.append(data_value)
+
+# print(list_of_ids[0])
