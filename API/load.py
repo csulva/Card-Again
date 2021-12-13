7bc8703714c234f55e6213752148d0451f0e857c
csulva <chrissulva1@gmail.com>
2021-12-12 23:36:15 -0500

    working on new function for getting card information

--- API/load.py
+++ API/load.py
@@ -3,19 +3,20 @@ import requests
 import json
 import time
 
-# list_of_ids = []
-# num = 1
-# while num < 59:
-#     BASE_URL = f'https://api.pokemontcg.io/v2/cards/?search=a&page={num}'
-#     request = requests.get(BASE_URL)
-#     data = request.json()
-#     for key, value in data.items():
-#         if key == 'data':
-#             for dicts in value:
-#                 for key_data, value_data in dicts.items():
-#                     if key_data == 'id':
-#                         list_of_ids.append(value_data)
-#     num += 1
+list_of_ids = []
+dict_of_important_card_information = {}
+num = 1
+while num < 59:
+    BASE_URL = f'https://api.pokemontcg.io/v2/cards/?search=a&page={num}'
+    request = requests.get(BASE_URL)
+    data = request.json()
+    for key, value in data.items():
+        if key == 'data':
+            for dicts in value:
+                for key_data, value_data in dicts.items():
+                    if key_data == 'id':
+                        list_of_ids.append(value_data)
+    num += 1
 
 
 with open('API/card_ids.json', 'r') as fin:
@@ -40,46 +41,46 @@ with open('API/card_ids.json', 'r') as fin:
 #     return data_list
 
 
-data_list = []
-index = 0
-for id in ids:
-    BASE_URL = f'https://api.pokemontcg.io/v2/cards/' + id
-    request = requests.get(BASE_URL)
-    request.raise_for_status()
-    if request.status_code != 204:
-        card_data = request.json()
-        for key, value in card_data.items():
-            if key == 'data':
-                for key_data, value_data in value.items():
-                    if key_data == 'id':
-                        data_list.append(value_data)
-    index += 1
-    if index == 100:
-        time.sleep(30)
-    if index == 200:
-        time.sleep(30)
-    if index == 300:
-        time.sleep(30)
-    if index == 400:
-        time.sleep(30)
-    if index == 500:
-        time.sleep(30)
-    if index == 600:
-        time.sleep(30)
-    if index == 700:
-        time.sleep(30)
-    if index == 800:
-        time.sleep(30)
-    if index == 900:
-        time.sleep(30)
-    if index == 1000:
-        time.sleep(30)
-    if index == 1100:
-        time.sleep(30)
-    if index == 1200:
-        time.sleep(30)
+# data_list = []
+# index = 0
+# for id in ids:
+#     BASE_URL = f'https://api.pokemontcg.io/v2/cards/' + id
+#     request = requests.get(BASE_URL)
+#     request.raise_for_status()
+#     if request.status_code != 204:
+#         card_data = request.json()
+#         for key, value in card_data.items():
+#             if key == 'data':
+#                 for key_data, value_data in value.items():
+#                     if key_data == 'id':
+#                         data_list.append(value_data)
+#     index += 1
+#     if index == 100:
+#         time.sleep(30)
+#     if index == 200:
+#         time.sleep(30)
+#     if index == 300:
+#         time.sleep(30)
+#     if index == 400:
+#         time.sleep(30)
+#     if index == 500:
+#         time.sleep(30)
+#     if index == 600:
+#         time.sleep(30)
+#     if index == 700:
+#         time.sleep(30)
+#     if index == 800:
+#         time.sleep(30)
+#     if index == 900:
+#         time.sleep(30)
+#     if index == 1000:
+#         time.sleep(30)
+#     if index == 1100:
+#         time.sleep(30)
+#     if index == 1200:
+#         time.sleep(30)
 
-print(len(data_list))
+# print(len(data_list))
 
 # print(get_api_items_first_level(ids, 'id'))
 # index = 0
