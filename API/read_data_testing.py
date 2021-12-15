79a6db3f7345ffbd731e36fbb1b569e852142289
csulva <chrissulva1@gmail.com>
2021-12-14 21:12:12 -0500

    looking at data sandbox

--- API/read_data_testing.py
+++ API/read_data_testing.py
@@ -3,4 +3,13 @@ import json
 with open('API/card_data.json', 'r') as fin:
     card_data = json.load(fin)
 
-print(card_data[5002])
+print(float(card_data[0]['price']['holofoil']['low']))
+
+# for info in card_data:
+#     try:
+#         if info['price']['normal']['low']:
+#             normal_price_low = info['price']['normal']['low']
+#         pass
+#     except:
+#         pass
+# print(normal_price_low)
