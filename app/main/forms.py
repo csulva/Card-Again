21bbe5114969d96ab906591a2d83aa3ea69993ce
csulva <chrissulva1@gmail.com>
2021-12-17 21:14:55 -0500

    search form

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, TextAreaField, SubmitField
+from wtforms import StringField, TextAreaField, SubmitField, SearchField
 from wtforms.validators import DataRequired, Length, ValidationError
 from app.models import User
 
@@ -9,3 +9,6 @@ class EditProfileForm(FlaskForm):
     about_me = TextAreaField('About me', validators=[Length(min=0, max=140)])
     submit = SubmitField('Submit')
 
+class SearchCardForm(FlaskForm):
+    search = SearchField('Search for cards...')
+    submit = SubmitField('Submit')
