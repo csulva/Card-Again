80bc2bfe16e289f3b4f91ab65d928cf7b6221b18
csulva <chrissulva1@gmail.com>
2021-12-31 16:14:49 -0500

    deploy additions

--- cardagain.py
+++ cardagain.py
@@ -1,6 +1,6 @@
 from app import create_app, db
 import os
-from flask_migrate import Migrate
+from flask_migrate import Migrate, upgrade
 from app.models import Role, User, Card
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
@@ -10,3 +10,15 @@ migrate = Migrate(app, db, render_as_batch=True)
 @app.shell_context_processor
 def make_shell_context():
     return dict(db=db, User=User, Card=Card, Role=Role)
+
+@app.cli.command()
+def deploy():
+    """ Run deployment tasks """
+    # migrate database
+    upgrade()
+
+    Role.insert_roles()
+
+    User.add_self_follows()
+
+    Card.insert_cards()
