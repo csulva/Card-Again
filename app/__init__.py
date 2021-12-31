f71875ff37046b78dd05f4fe4c294432e880fc54
csulva <chrissulva1@gmail.com>
2021-12-31 16:14:34 -0500

    last deployment additions

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,8 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_moment import Moment
 from flask_mail import Mail
+from flask_wtf.csrf import CSRFProtect
+
 
 bootstrap = Bootstrap()
 db = SQLAlchemy()
@@ -12,6 +14,8 @@ login = LoginManager()
 login.login_view = 'auth.login'
 moment = Moment()
 mail = Mail()
+csrf = CSRFProtect()
+
 
 
 def create_app(config_name='default'):
@@ -25,11 +29,26 @@ def create_app(config_name='default'):
     login.init_app(app)
     moment.init_app(app)
     mail.init_app(app)
+    csrf.init_app(app)
+
 
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
     from .auth import auth as auth_blueprint
     app.register_blueprint(auth_blueprint)
+
+    if app.config['HTTPS_REDIRECT']:
+            from flask_talisman import Talisman
+            Talisman(app, content_security_policy={
+                    'default-src': [
+                        "'self'",
+                        'cdnjs.cloudflare.com',
+                    ],
+                    # allow images from anywhere,
+                    #   including unicornify.pictures
+                    'img-src': '*'
+                }
+            )
 
     return app
