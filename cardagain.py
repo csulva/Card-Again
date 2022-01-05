932f4fcf85d3b8eb62217e63de0a358d55c83bcf
csulva <chrissulva1@gmail.com>
2022-01-04 19:01:14 -0500

    code

--- cardagain.py
+++ cardagain.py
@@ -3,6 +3,7 @@ import os
 from flask_migrate import Migrate, upgrade
 from app.models import Role, User, Card
 
+
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 
 migrate = Migrate(app, db, render_as_batch=True)
