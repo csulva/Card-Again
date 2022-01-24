58fd03456f1b30759e75ad823926d1198bc54ea1
csulva <chrissulva1@gmail.com>
2022-01-23 22:50:51 -0500

    comments

--- cardagain.py
+++ cardagain.py
@@ -4,17 +4,21 @@ from flask_migrate import Migrate, upgrade
 from app.models import Follow, Role, User, Card
 import logging
 
+# Create app from FLASK_CONFIG environment variable config or default config
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 
 migrate = Migrate(app, db, render_as_batch=True)
 
 @app.shell_context_processor
 def make_shell_context():
+    """Provides database tables/classes for flask shell sessions
+    so they do not need to be called every time
+    """
     return dict(db=db, User=User, Card=Card, Role=Role, Follow=Follow)
 
 @app.cli.command()
 def deploy():
-    """ Run deployment tasks """
+    """Run deployment tasks for production"""
     # migrate database
     upgrade()
 
