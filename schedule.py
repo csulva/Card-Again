84321203cc0429bd97e50946afdaab83f5d5f1ab
csulva <chrissulva1@gmail.com>
2022-01-24 23:24:21 -0500

    working on fixing schdeduler/import error

--- schedule.py
+++ schedule.py
@@ -1,15 +1,15 @@
-from flask_apscheduler import APScheduler
-from API.load import load_cards
-from app.models import Card
+# from flask_apscheduler import APScheduler
+# from API.load import load_cards
+# from app.models import Card
 
-scheduler = APScheduler()
+# scheduler = APScheduler()
 
-@scheduler.task('interval', id='update_cards', seconds=10)
-def update_cards():
+# @scheduler.task('interval', id='update_cards', seconds=10)
+# def update_cards():
 
-    # load_cards()
-    # print('cards loaded')
-    # Card.insert_cards()
-    # print('new cards inserted into db')
-    # Card.update_cards()
-    print('cards updated')
+#     # load_cards()
+#     # print('cards loaded')
+#     # Card.insert_cards()
+#     # print('new cards inserted into db')
+#     # Card.update_cards()
+#     print('cards updated')
