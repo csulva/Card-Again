210991d2d6f2303a5d39b60ed9226ec1b07e7d1d
csulva <chrissulva1@gmail.com>
2022-01-25 22:52:01 -0500

    adding app context processor to main blueprint

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -1,5 +1,13 @@
 from flask import Blueprint
+from app.models import Permission
 
 main = Blueprint('main', __name__)
+
+@main.app_context_processor
+def inject_permissions():
+    """Access the Permission database table/class without needing to call it every time
+    you need to pass it into functions.
+    """
+    return dict(Permission=Permission)
 
 from . import views, forms, errors
