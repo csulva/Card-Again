d953f89d214f324c1b058619411e4d94821525dc
csulva <chrissulva1@gmail.com>
2022-01-25 21:42:54 -0500

    decorators docstrings

--- app/decorators.py
+++ app/decorators.py
@@ -4,6 +4,12 @@ from flask_login import current_user
 from .models import Permission
 
 def permission_required(permission):
+    """Function decorator used so that only those
+    who have been confirmed can access
+
+    Args:
+        permission (int): The permission given based on the list in Permission class from app.models
+    """
     def decorator(f):
         @wraps(f)
         def decorated_function(*args, **kwargs):
@@ -14,4 +20,6 @@ def permission_required(permission):
     return decorator
 
 def admin_required(f):
+    """Function decorator used so that only admins can access
+    """
     return permission_required(Permission.ADMIN)(f)
