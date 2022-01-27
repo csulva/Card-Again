509702550a957e992a76c572d6ce3e1122bedf38
csulva <chrissulva1@gmail.com>
2022-01-27 16:30:00 -0500

    specifying dates when tasks should run

--- app/tasks.py
+++ app/tasks.py
@@ -1,15 +1,21 @@
 from API.load import load_cards
 from app.models import Card
 from app import scheduler
-# from flask import current_app
-# app=current_app._get_current_object()
-# with app.app_context():
+from apscheduler.events import EVENT_JOB_EXECUTED, EVENT_JOB_ERROR
+import logging
 
-@scheduler.task('interval', id='update_cards', seconds=30)
+logging.basicConfig()
+logging.getLogger('apscheduler').setLevel(logging.DEBUG)
+
+
+@scheduler.task('interval', id='update_cards', day_of_week=(0,4), hour=0)
 def update_cards():
-    # load_cards()
-    print('test cards loaded')
-    Card.insert_cards()
-    print('new cards inserted into db')
-    Card.update_cards()
-    print('cards updated')
+    with scheduler.app.app_context():
+        load_cards()
+        print('test cards loaded')
+        Card.insert_cards()
+        print('new cards inserted into db')
+        Card.update_cards()
+        print('cards updated')
+
+scheduler.add_listener(update_cards, EVENT_JOB_EXECUTED | EVENT_JOB_ERROR)
