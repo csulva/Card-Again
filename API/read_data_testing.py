979cbc0678e649f7238fce02333c9d948bfbeb37
csulva <chrissulva1@gmail.com>
2022-01-26 23:06:03 -0500

    comments

--- API/read_data_testing.py
+++ API/read_data_testing.py
@@ -1,3 +1,5 @@
+# Testing reading from card_data.json file
+
 import json
 
 with open('API/card_data.json', 'r') as fin:
@@ -5,12 +7,3 @@ with open('API/card_data.json', 'r') as fin:
 
 print(card_data[1]['set_name'])
 print(card_data[1]['set_series'])
-
-# for info in card_data:
-#     try:
-#         if info['price']['normal']['low']:
-#             normal_price_low = info['price']['normal']['low']
-#         pass
-#     except:
-#         pass
-# print(normal_price_low)
